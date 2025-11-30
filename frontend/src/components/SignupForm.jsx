8ee18e105e71ca7ba23ad0c5c967d23291ea5b0c
krish <krishsoni7879903039@gmail.com>
2025-11-30 14:24:01 +0530

    api fix in signup component

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -28,7 +28,7 @@ const SignupForm = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/signup",
+        "https://youtube-watchtogether.onrender.com/api/auth/signup",
         {
           username: formData.username.trim(),
           email: formData.email.trim().toLowerCase(),
